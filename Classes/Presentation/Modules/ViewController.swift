e0420422effa0018c17c31af1ca5ed5360c7ff44
Anton Kovalev <anton.kovalev@rosberry.com>
2018-10-31 14:18:25 +0600

    Initial Commit

---
+++ Classes/Presentation/Modules/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2018 Rosberry. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
