de2e5555f11c50845d3ec7654548ea46e726a46f
Anton Kovalev <anton.kovalev@rosberry.com>
2018-11-01 09:32:26 +0600

    Fix event description

--- Classes/Core/Analog/Logger/Event.swift
+++ Classes/Core/Analog/Logger/Event.swift
@@ -6,10 +6,10 @@ import Foundation
 
 public final class Event: Codable {
     private let title: String
-    private let parameters: [String: String]
+    private let parameters: [String: String]?
     private let date: Date
     
-    init(title: String, parameters: [String: String]) {
+    public init(title: String, parameters: [String: String]? = nil) {
         self.title = title
         self.parameters = parameters
         self.date = .init()
@@ -23,8 +23,9 @@ extension Event: AttributedStringConvertible {
     public var description: NSAttributedString {
         let formatter = DateFormatter()
         formatter.dateStyle = .short
+        formatter.timeStyle = .long
         
-        let parametersDescriptions = parameters.map { key, value in
+        let parametersDescriptions = parameters?.map { key, value in
             return "\(key): \(value)"
         }
         
@@ -33,20 +34,18 @@ extension Event: AttributedStringConvertible {
         let dateString = NSAttributedString(string: "✏️ [\(formatter.string(from: date))]",
                                             attributes: [.font: UIFont.systemFont(ofSize: fontSize)])
         let finalString = NSMutableAttributedString(attributedString: dateString)
-        finalString.append(lineBreakString(fontSize: fontSize))
         let titleString = NSAttributedString(string: "\(title)",
                                              attributes: [.font: UIFont.boldSystemFont(ofSize: fontSize)])
         finalString.append(lineBreakString(fontSize: fontSize))
         finalString.append(titleString)
         
-        if parametersDescriptions.count > 0 {
+        if let parametersDescriptions = parametersDescriptions,
+           parametersDescriptions.count > 0 {
             let parametersString = NSAttributedString(string: parametersDescriptions.joined(separator: "\n"),
                                                       attributes: [.font: UIFont.systemFont(ofSize: fontSize)])
             finalString.append(lineBreakString(fontSize: fontSize))
             finalString.append(parametersString)
         }
-        
-        finalString.append(dateString)
         
         return finalString
     }
